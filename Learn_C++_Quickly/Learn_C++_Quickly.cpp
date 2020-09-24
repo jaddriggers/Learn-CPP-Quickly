377cf56de9c92b535f2327ae8c0ca962637604da
Jad Driggers <jaddriggers@gmail.com>
2020-09-24 12:38:44 -0400

    Inital Commit

---
+++ Learn_C++_Quickly/Learn_C++_Quickly.cpp
@@ -0,0 +1,13 @@
+// Learn_C++_Quickly.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int age{};
+	cout << "How old are you?" << endl;
+	cin >> age;
+	cout << "wow, we are the same age! I am also " << age << " years old!" << endl;
+}
